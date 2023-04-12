ccbbc319e2f527385463171bb4485d4a18f5d7e9
lqs <sdulqs@163.com>
2023-04-12 15:23:44 +0800

    make putcgetc work, some adjustment

--- intro/mycat.c
+++ intro/mycat.c
@@ -10,9 +10,9 @@ int main(int argc, char *argv[])
     while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
         if (write(STDOUT_FILENO, buf, n) != n)
             err_sys("write error");
-    
+
     if (n < 0)
         err_sys("read error");
-    
+
     exit(0);
 }
