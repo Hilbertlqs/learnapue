409e0cd046d94514f5784472310d4846c6a2bf28
lqs <sdulqs@163.com>
2023-04-18 11:49:54 +0800

    modify seek.c

--- fileio/seek.c
+++ fileio/seek.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    if (lseek(STDIN_FILENO, 0, SEEK_SET) == -1)
+    if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1)
         printf("cannot seek\n");
     else
         printf("seek OK\n");
