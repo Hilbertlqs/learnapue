46c582e5260ceffe269022e0c8f0b7886ed9f858
lqs <sdulqs@163.com>
2023-09-07 13:50:27 +0800

    threads

---
+++ threads/maketimeout.c
@@ -0,0 +1,12 @@
+#include <sys/time.h>
+#include <stdlib.h>
+
+void maketimeout(struct timespec *tsp, long minutes)
+{
+    struct timeval now;
+
+    gettimeofday(&now, NULL);
+    tsp->tv_sec = now.tv_sec;
+    tsp->tv_nsec = now.tv_usec * 1000;
+    tsp->tv_sec += minutes * 60;
+}
